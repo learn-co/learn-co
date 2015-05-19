9af995d26d54e57e65127801f1612b8586e612c2
Logan Hasson <logan.hasson@gmail.com>
2015-05-19 16:23:05 -0400

    Add better escaping

--- lib/learn/cli.rb
+++ lib/learn/cli.rb
@@ -6,15 +6,11 @@ module Learn
 
       You can supply the following options when running Jasmine tests:
 
-      -n, --[no-]color  # Turn off color output
-
-      -l, --local       # Don't push results to Learn
-
-      -b, --browser     # Run tests in browser
-
-      -o, --out FILE    # Specify an output file for your test results
-
-      -s, --skip        # Don't run dependency checks
+      \x20 -n, --[no-]color  # Turn off color output
+      \x5 -l, --local       # Don't push results to Learn
+      \x5 -b, --browser     # Run tests in browser
+      \x5 -o, --out FILE    # Specify an output file
+      \x5 -s, --skip        # Don't run dependency checks
 
       When running an RSpec test suite, all normal RSpec options can be
       passed in.
