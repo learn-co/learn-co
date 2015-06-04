db3ce38ea5a79cca7175ba31d87a970c29cdd217
Logan Hasson <logan.hasson@gmail.com>
2015-06-04 11:25:50 -0400

    Move test arg sanitization into class

--- lib/learn/cli.rb
+++ lib/learn/cli.rb
@@ -6,7 +6,7 @@ module Learn
 
       You can supply the following options when running Jasmine tests:
 
-      -n, --[no-]color  # Turn off color output
+      --no-color            # Turn off color output
       \x5 -l, --local       # Don't push results to Learn
       \x5 -b, --browser     # Run tests in browser
       \x5 -o, --out FILE    # Specify an output file
--- lib/learn/options_sanitizer.rb
+++ lib/learn/options_sanitizer.rb
@@ -11,8 +11,51 @@ module Learn
     end
 
     def sanitize!
+      sanitize_editor_arg!
+      sanitize_test_args!
+    end
+
+    private
+
+    def sanitize_editor_arg!
       args.map! do |arg|
         SANITIZE_LIST[arg] ? SANITIZE_LIST[arg] : arg
+      end
+    end
+
+    def sanitize_test_args!
+      if args.empty? || args.none? {|arg| KNOWN_COMMANDS.include?(arg)}
+        if args[0] && !args[0].start_with?('-')
+          puts "Unknown command: #{args[0]}. Type `learn help` to see what you can do."
+          exit
+        elsif args.any? {|arg| ['-o', '--out'].include?(arg)}
+          index = args.index('-o') || args.index('--out')
+          if args[index+1] && !args[index+1].start_with?('-')
+            out_arg = "#{args[index]} #{args[index+1]}"
+            args.delete(args[index])
+            args.delete(args[index+1])
+
+            if args.all? {|arg| KNOWN_TEST_FLAGS.include?(arg)}
+              args.unshift('test')
+              args.push(out_arg)
+            else
+              unknown_flags = args.select {|arg| !KNOWN_TEST_FLAGS.include?(arg)}
+              puts "Unknown #{unknown_flags.count > 1 ? 'flags' : 'flag'}: #{unknown_flags.join(', ')}"
+              exit
+            end
+          else
+            puts "Must specify an output file when using the -o, --out flag."
+            exit
+          end
+        elsif args.all? {|arg| arg.start_with?('-')}
+          args.unshift('test')
+        else
+          puts "What?"
+          exit
+        end
+      elsif args[0] == 'test' && args[1] && !args[1].start_with?('-')
+        puts "Unknown flag: #{args[1]}"
+        exit
       end
     end
   end
